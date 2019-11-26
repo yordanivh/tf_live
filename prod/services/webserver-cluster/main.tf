6182afa31bf37f8e94296ec6e6d15ee036481a63
YordanIH <yordan@hashicorp.com>
2019-11-26 15:07:33 +0200

    Commit the made changes

--- prod/services/webserver-cluster/main.tf
+++ prod/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/service/webserver-cluster"
+  source = "github.com/yordanivh/tf_modules//modules/service/webserver-cluster?ref=v0.0.1"
 
   cluster_name           = "webserver-prod"
   db_remote_state_bucket = "terraform-book-bucket"
