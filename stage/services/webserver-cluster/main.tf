389bf24f81770a00e4e9965ec9b8d4e19fb1d5f8
YordanIH <yordan@hashicorp.com>
2019-11-29 09:46:37 +0200

    moving on with exercises

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -3,14 +3,15 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../../tf_modules/modules/service/webserver-cluster"
+  source                 = "../../../../tf_modules/modules/service/webserver-cluster"
   cluster_name           = "webserver-stage"
   db_remote_state_bucket = "terraform-book-bucket"
   db_remote_state_key    = "stage/data-stores/mysql/terraform.tfstate"
 
-  instance_type = "t2.micro"
-  min_size      = 2
-  max_size      = 2
+  instance_type      = "t2.micro"
+  min_size           = 2
+  max_size           = 2
+  enable_autoscaling = false
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
